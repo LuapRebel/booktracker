78f2e0fe14c7d7b7b399fad074f3f56ccc284921
Paul Leber <luaprebel@gmail.com>
2025-02-13 19:12:43 -0800

    update styling in stats tables

--- src/stats.py
+++ src/stats.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from statistics import mean
 
+from rich.text import Text
 from textual.app import ComposeResult
 from textual.containers import Center
 from textual.screen import Screen
@@ -95,30 +96,41 @@ class BookStatsScreen(Screen):
             yield Label("BookTracker Yearly Stats", id="stats-table-year-label")
             yield DataTable(id="stats-table-year")
         yield Rule(line_style="heavy")
-        with Center(id="complete-stats"):
-            yield Label("BookTracker Complete Stats", id="stats-table-complete-label")
-            yield DataTable(id="stats-table-complete")
+        with Center(id="detailed-stats"):
+            yield Label("BookTracker Detailed Stats", id="stats-table-detailed-label")
+            yield DataTable(id="stats-table-detailed")
         yield Footer()
 
     def on_mount(self) -> None:
-        # books = load_books()
-        complete_stats = BookStats(self.books).complete_stats()
-        years = {stat["year"] for stat in complete_stats}
+        detailed_stats = BookStats(self.books).detailed_stats()
+        detailed_table = self.query_one("#stats-table-detailed", DataTable)
+        detailed_table.clear(columns=True)
+        detailed_columns = [
+            self._style_table_column(key) for key in detailed_stats[0].keys()
+        ]
+        detailed_table.add_columns(*detailed_columns)
+        detailed_rows = [stat.values() for stat in detailed_stats]
+        for row in detailed_rows:
+            styled_row = [Text(str(cell), justify="center") for cell in row]
+            detailed_table.add_row(*styled_row)
+        detailed_table.zebra_stripes = True
+        years = {stat["year"] for stat in detailed_stats}
         year_table = self.query_one("#stats-table-year", DataTable)
-        year_table_stats = [BookStats(self.books).year_stats(year)[0] for year in years]
-        year_table_columns = year_table_stats[0].keys()
-        year_table_rows = [stat.values() for stat in year_table_stats]
         year_table.clear(columns=True)
+        year_table_stats = [BookStats(self.books).year_stats(year) for year in years]
+        year_table_columns = [
+            self._style_table_column(key) for key in year_table_stats[0].keys()
+        ]
         year_table.add_columns(*year_table_columns)
-        year_table.add_rows(year_table_rows)
+        year_table_rows = [stat.values() for stat in year_table_stats]
+        for row in year_table_rows:
+            styled_row = [Text(str(cell), justify="center") for cell in row]
+            year_table.add_row(*styled_row)
         year_table.zebra_stripes = True
-        complete_columns = complete_stats[0].keys()
-        complete_rows = [stat.values() for stat in complete_stats]
-        complete_table = self.query_one("#stats-table-complete", DataTable)
-        complete_table.clear(columns=True)
-        complete_table.add_columns(*complete_columns)
-        complete_table.add_rows(complete_rows)
-        complete_table.zebra_stripes = True
+
+    def _style_table_column(self, column: str) -> Text:
+        padded_title = column.replace("_", " ").title().center(len(column) + 2)
+        return Text(padded_title, justify="center")
 
     def action_push_books(self) -> None:
         self.app.push_screen("books")
