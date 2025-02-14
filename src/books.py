614b65814a5e7cb85d56680d5d2d32f08cbb447e
Paul Leber <luaprebel@gmail.com>
2025-02-14 04:23:03 -0800

    Move BookStatsScreen to books.py to avoid DataTable formatting issue on push_screen

--- src/books.py
+++ src/books.py
@@ -1,7 +1,8 @@
 from pydantic import ValidationError
+from rich.text import Text
 from textual import on
 from textual.app import ComposeResult
-from textual.containers import Container, Horizontal
+from textual.containers import Center, Container, Horizontal
 from textual.screen import ModalScreen, Screen
 from textual.widget import Widget
 from textual.widgets import (
@@ -10,12 +11,14 @@ from textual.widgets import (
     Footer,
     Header,
     Input,
+    Label,
+    Rule,
     Static,
 )
 
 from db import db
 from schema import Book
-from stats import BookStatsScreen
+from stats import BookStats
 
 
 def load_books() -> list[Book]:
@@ -273,6 +276,61 @@ class BookFilterScreen(Screen):
 
     def action_push_books(self) -> None:
         self.clear_inputs()
+        self.app.push_screen(BookScreen())
+
+
+class BookStatsScreen(Screen):
+    """Screen to display stats about books read"""
+
+    BINDINGS = [("escape", "push_books", "Books")]
+
+    def __init__(self, books: list[Book]) -> None:
+        super().__init__()
+        self.books = books
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        with Center(id="yearly-stats"):
+            yield Label("BookTracker Yearly Stats", id="stats-table-year-label")
+            yield DataTable(id="stats-table-year")
+        yield Rule(line_style="heavy")
+        with Center(id="detailed-stats"):
+            yield Label("BookTracker Detailed Stats", id="stats-table-detailed-label")
+            yield DataTable(id="stats-table-detailed")
+        yield Footer()
+
+    def on_mount(self) -> None:
+        detailed_stats = BookStats(self.books).detailed_stats()
+        detailed_table = self.query_one("#stats-table-detailed", DataTable)
+        detailed_table.clear(columns=True)
+        detailed_columns = [
+            self._style_table_column(key) for key in detailed_stats[0].keys()
+        ]
+        detailed_table.add_columns(*detailed_columns)
+        detailed_rows = [stat.values() for stat in detailed_stats]
+        for row in detailed_rows:
+            styled_row = [Text(str(cell), justify="center") for cell in row]
+            detailed_table.add_row(*styled_row)
+        detailed_table.zebra_stripes = True
+        years = {stat["year"] for stat in detailed_stats}
+        year_table = self.query_one("#stats-table-year", DataTable)
+        year_table.clear(columns=True)
+        year_table_stats = [BookStats(self.books).year_stats(year) for year in years]
+        year_table_columns = [
+            self._style_table_column(key) for key in year_table_stats[0].keys()
+        ]
+        year_table.add_columns(*year_table_columns)
+        year_table_rows = [stat.values() for stat in year_table_stats]
+        for row in year_table_rows:
+            styled_row = [Text(str(cell), justify="center") for cell in row]
+            year_table.add_row(*styled_row)
+        year_table.zebra_stripes = True
+
+    def _style_table_column(self, column: str) -> Text:
+        padded_title = column.replace("_", " ").title().center(len(column) + 2)
+        return Text(padded_title, justify="center")
+
+    def action_push_books(self) -> None:
         self.app.push_screen(BookScreen())
 
 
