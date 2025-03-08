7bbbdb6b647058dd8e3f8f9af471bc82005656ff
Paul Leber <luaprebel@gmail.com>
2025-03-08 11:43:59 -0800

    Setup logging for CUD operations

--- src/app.py
+++ src/app.py
@@ -1,8 +1,24 @@
+import json
+import logging
+import logging.config
+import logging.handlers
+from pathlib import Path
+
+
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header
 
 from books import BookScreen
 from db import db
+
+logger = logging.getLogger("booktracker")
+
+
+def setup_logging():
+    config_file = Path("src/config.json")
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+    logging.config.dictConfig(config)
 
 
 class BookTracker(App):
@@ -28,5 +44,8 @@ class BookTracker(App):
 
 
 if __name__ == "__main__":
+    setup_logging()
+    logger.info("Starting Booktracker...")
     app = BookTracker()
     app.run()
+    logger.info("Exiting Booktracker...")
